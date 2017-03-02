d27a9bacae3ffb4bfd3ab337978ef6016df43f6b
Alexander Trauzzi <atrauzzi@gmail.com>
2017-03-01 20:59:22 -0600

    fix: Remove leftover postal reference.
     - Now sure how it snuck through.

--- src/Engine/DriverStatic.ts
+++ src/Engine/DriverStatic.ts
@@ -1,7 +1,7 @@
 import {Driver} from "./Driver";
-
+import {Service as Bus} from "../Bus/Service";
 
 export interface DriverStatic<T extends Driver> {
 
-    new(bus: IPostal, options: {[property: string]: any}): T;
+    new(bus: Bus, options: {[property: string]: any}): T;
 }
